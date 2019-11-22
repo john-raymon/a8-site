796dfd16d005ec9f1d515e0e9e4485af98d7f9c7
John Ray <john@trychec.com>
2019-11-21 20:55:47 -0500

    add page

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "88448c05d9ea2604c4c4";
+/******/ 	var hotCurrentHash = "0a44d8539f94de8aa07d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
